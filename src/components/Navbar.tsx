8083474b207b89604062954c64e646579abfb7f2
HerbertNtim <herbertntim15@gmail.com>
2025-05-09 17:35:35 +0000

    [02-Navbar]: developed mobile navbar using shadcn dropdown component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,14 @@
 import Link from 'next/link'
 import React from 'react'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { MenuIcon } from 'lucide-react'
 
 const navItems = [
   { name: 'Profile', path: '/profile' },
@@ -12,19 +21,41 @@ const navItems = [
 
 const Navbar = () => {
   return (
-    <header className='w-full h-20 bg-blue-400'>
+    <header className='w-full h-20 bg-blue-700'>
       <div className='container mx-auto flex items-center justify-between h-full'>
-        <Link href="/" className='text-white text-2xl font-bold'>ParkMe</Link>
-        <nav className='hidden sm:flex items-center mr-4'>
+        <Link href="/" className='text-white text-2xl font-bold sm:ml-0 ml-3'>ParkMe</Link>
+        <nav className='hidden lg:flex items-center mr-4'>
           {navItems.map((item) => (
             <Link key={item.name} href={item.path} className='text-white text-xl font-medium mx-4 hover:scale-90 transition-transform duration-100'>
               {item.name}
             </Link>
           ))}
         </nav>
+        <MobileNavbar />
       </div>
     </header>
   )
 }
 
 export default Navbar
+
+
+const MobileNavbar = () => {
+  return (
+    <div className='lg:hidden flex items-center mr-6 cursor-pointer relative'>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <MenuIcon className='size-8 text-white'/>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className='w-48 h-80 absolute right-0 top-0 p-4 bg-gray-100 shadow-lg rounded-md'>
+          <DropdownMenuLabel>My Profile</DropdownMenuLabel>
+          <DropdownMenuSeparator className="bg-gray-700" />
+          <DropdownMenuItem className="hover:scale-90 transition-transform duration-100 curser-pointer text-lg">Profile</DropdownMenuItem>
+          <DropdownMenuItem className="hover:scale-90 transition-transform duration-100 curser-pointer text-lg">Billing</DropdownMenuItem>
+          <DropdownMenuItem className="hover:scale-90 transition-transform duration-100 curser-pointer text-lg">Team</DropdownMenuItem>
+          <DropdownMenuItem className="hover:scale-90 transition-transform duration-100 curser-pointer text-lg">Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  )
+}
