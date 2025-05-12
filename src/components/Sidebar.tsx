892cd9e020b9d6a8439b795f66a8592685943f57
HerbertNtim <herbertntim15@gmail.com>
2025-05-12 17:16:10 +0000

    dev: finish up the sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,21 @@
 import Link from 'next/link'
 import React from 'react'
+import ActiveLink from './ActiveLink'
+
+const SidebarItem = [
+  {
+    name: "Location",
+    href: "/dashboard/location/tileview",
+  },
+  {
+    name: 'Bookings',
+    href: '/dashboard/bookings',
+  },
+  {
+    name: 'Revenue',
+    href: '/dashboard/revenue',
+  }
+]
 
 const Sidebar = () => {
   return (
@@ -9,6 +25,19 @@ const Sidebar = () => {
           Parkme
         </Link>
       </h1>
+      <div className='flex flex-col justify-between h-full'>
+        <ul className='w-full flex flex-col space-y-2 pt-8 text-2xl'>
+          {SidebarItem.map((link) => (
+            <li key={link.href}>
+              <ActiveLink href={link.href}>{link.name}</ActiveLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className='pl-4 text-blue-600'>
+        UserButton
+      </div>
     </div>
   )
 }
